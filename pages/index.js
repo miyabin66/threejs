3f0d5bf83388e295379cb557afc0c53a6646b43c
miyabin66 <freezemark5166@gmail.com>
2021-04-12 22:08:29 +0900

    ジオメトリの基本

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,16 @@ import {
   MeshStandardMaterial,
   Mesh,
   DirectionalLight,
-  TextureLoader
+  Vector3,
+  Object3D,
+  DoubleSide,
+  AmbientLight,
+  BoxGeometry,
+  PlaneGeometry,
+  TetrahedronGeometry,
+  ConeGeometry,
+  CylinderGeometry,
+  TorusGeometry
 } from 'three'
 
 export default function Home() {
@@ -33,33 +42,54 @@ export default function Home() {
 
     // カメラを作成
     const camera = new PerspectiveCamera(45, width / height)
-    camera.position.set(0, 0, +1000)
-
-    // 球体を作成
-    const geometry = new SphereGeometry(300, 30, 30)
-    // 画像を読み込む
-    const loader = new TextureLoader()
-    const texture = loader.load('/earthmap1k.jpg')
-    // マテリアルにテクスチャーを設定
+    camera.position.set(0, 500, +1000)
+    camera.lookAt(new Vector3(0, 0, 0))
+    
+    const container = new Object3D()
+    scene.add(container)
+  
+    // マテリアルを作成
     const material = new MeshStandardMaterial({
-      map: texture
+      color: 0xff0000,
+      side: DoubleSide
     })
-    // メッシュを作成
-    const mesh = new Mesh(geometry, material)
-    // 3D空間にメッシュを追加
-    scene.add(mesh)
-
-    // 平行光源
+  
+    // 平行光源を作成
     const directionalLight = new DirectionalLight(0xFFFFFF)
     directionalLight.position.set(1, 1, 1)
-    // シーンに追加
     scene.add(directionalLight)
+    // 環境光を作成
+    const ambientLight = new AmbientLight(0x999999)
+    scene.add(ambientLight)
+  
+    // ジオメトリを作成
+    const geometryList = [
+      new SphereGeometry(50), // 球体
+      new BoxGeometry(100, 100, 100), // 直方体
+      new PlaneGeometry(100, 100), // 平面
+      new TetrahedronGeometry(100, 0), // カプセル形状
+      new ConeGeometry(100, 100, 32), // 三角錐
+      new CylinderGeometry(50, 50, 100, 32), // 円柱
+      new TorusGeometry(50, 30, 16, 100) // ドーナツ形状
+    ]
+    
+    geometryList.map((geometry, index) => {
+      // 形状とマテリアルからメッシュを作成します
+      const mesh = new Mesh(geometry, material)
+  
+      // 3D表示インスタンスのsceneプロパティーが3D表示空間となります
+      container.add(mesh)
+  
+      // 円周上に配置
+      mesh.position.x = 400 * Math.sin((index / geometryList.length) * Math.PI * 2)
+      mesh.position.z = 400 * Math.cos((index / geometryList.length) * Math.PI * 2)
+    })
 
     tick()
 
     // 毎フレーム時に実行されるループイベントです
     function tick() {
-      mesh.rotation.y += 0.01
+      container.rotation.y += 0.01
       // レンダリング
       renderer.render(scene, camera)
 
@@ -67,6 +97,6 @@ export default function Home() {
     }
   }
   return (
-    <canvas id="myCanvas"></canvas>
+    <canvas id="myCanvas" />
   )
 }
