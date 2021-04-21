ee32e735b94802848deda20cd94c4e35585ce453
miyabin66 <freezemark5166@gmail.com>
2021-04-22 08:58:57 +0900

    さまざまなマテリアル-MeshLambertMaterial

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,10 @@ import {
   PerspectiveCamera,
   Mesh,
   TorusGeometry,
-  MeshNormalMaterial,
+  MeshLambertMaterial,
+  DirectionalLight,
+  PointLight,
+  PointLightHelper,
 } from 'three'
 
 export default function Home() {
@@ -36,19 +39,37 @@ export default function Home() {
     // ドーナツを作成
     const geometry = new TorusGeometry(300, 100, 64, 100)
     // マテリアルを作成
-    const material = new MeshNormalMaterial()
+    const material = new MeshLambertMaterial({ color: 0x6699ff })
     // メッシュを作成
     const mesh = new Mesh(geometry, material)
     // 3D空間にメッシュを追加
     scene.add(mesh)
+  
+    // 平行光源
+    const directionalLight = new DirectionalLight(0xffffff)
+    directionalLight.position.set(1, 1, 1)
+    scene.add(directionalLight)
+  
+    // ポイント光源
+    const pointLight = new PointLight(0xffffff, 2, 1000)
+    scene.add(pointLight)
+    const pointLightHelper = new PointLightHelper(pointLight, 30)
+    scene.add(pointLightHelper)
     
-    tick();
+    tick()
     
     // 毎フレーム時に実行されるループイベントです
     function tick() {
       // メッシュを回転させる
       mesh.rotation.x += 0.01
       mesh.rotation.y += 0.01
+  
+      // ライトを周回させる
+      pointLight.position.set(
+        500 * Math.sin(Date.now() / 500),
+        500 * Math.sin(Date.now() / 1000),
+        500 * Math.cos(Date.now() / 500)
+      )
       
       // レンダリング
       renderer.render(scene, camera)
